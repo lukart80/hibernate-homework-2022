f078478f1cebf80473b8df0b5ac4d5929a4c6572
artem <lukart80@yandex.ru>
2022-02-11 12:56:37 +0300

    parameter added

--- src/main/java/ru/hh/school/dao/EmployerDao.java
+++ src/main/java/ru/hh/school/dao/EmployerDao.java
@@ -18,7 +18,8 @@ public class EmployerDao extends GenericDao {
    */
   public Employer getEager(int employerId) {
     return getSession()
-        .createQuery("from Employer employer join fetch employer.vacancies where employer.id = " + employerId, Employer.class)
+        .createQuery("from Employer employer join fetch employer.vacancies where employer.id =:emplpuerId", Employer.class)
+            .setParameter("employerId", employerId)
         .getSingleResult();
   }
 
